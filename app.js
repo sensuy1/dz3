95da1899820abcf946e89d5b280cf400c9609df8
sensuy1 <sultanbejsenbekov@gmail.com>
2023-11-14 15:39:14 +0600

    dz3

---
+++ app.js
@@ -0,0 +1,39 @@
+// var teg = ['li', 'div', 'li', 'p', 'h1', 'p', 'h1', 'div', 'div', 'li', 'h1',  'p', 'h1', 'ol', 'br'];
+// var teg2 = {}
+// for (var i = 0; i < teg.length; i++){
+//     if (teg2[teg[i]]){
+//         teg2[teg[i]]++
+//     }else {
+//         teg2[teg[i]] = 1
+//     }
+// }
+// console.log(teg2)
+
+
+// var starbucksMenu = {
+//   coffee: {
+//     americano: 'Наслаждайтесь богатым вкусом классического американо с легким оттенком воды',
+//     latte: 'Кофе с молоком и небольшим количеством пены',
+//     espresso: 'Крепкий кофе, приготовленный в высокотехнологичной кофеварке',
+//     capuccino: 'Итальянский напиток сровными долями эспрессо, молока и пены',
+//   },
+//   tea: {
+//     blacktea: 'Классический черный чай',
+//     greentea: 'Освежающий зелёный чай, который прорадует вас богатством антиоксидантов',
+//     chamomileTea: 'Ромашковый чай, успокаивающий и расслабляющий',
+//     matchaLatte: 'Уникальный вкус молотого японского зеленого чая в каждом матча латте',
+//   },
+// }
+//
+//
+//
+// var coffeeAndTea = prompt('Введите название напитков Tea/Coffee').toLowerCase()
+//
+//     if (starbucksMenu.coffee[coffeeAndTea]){
+//         alert(`ваш заказ принят:  ${starbucksMenu.coffee[coffeeAndTea]}`)
+//     }else if(starbucksMenu.tea[coffeeAndTea]){
+//         alert(`Ваш заказ принят:  ${starbucksMenu.tea[coffeeAndTea]}`)
+//     }else {
+//         alert(`error`)
+//     }
+
